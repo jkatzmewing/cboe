85382a523a7ae1e33d02783d13423814414bbe55
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2021-05-20 10:26:38 -0400

    Clang format

--- scenedit/edsound.h
+++ scenedit/edsound.h
@@ -1,5 +1,4 @@
-void load_sounds ();
-void play_sound(short which) ; // if < 0, play asynch
+void load_sounds();
+void play_sound(short which); // if < 0, play asynch
 void force_play_sound(short which);
 void sound_pause(long len);
-
--- scenedit/global.cpp
+++ scenedit/global.cpp
@@ -11,108 +11,91 @@ extern char town_strs[180][256];
 extern Boolean give_delays;
 unsigned long anim_ticks = 0;
 BOOL do_choose_anim = FALSE;
-BOOL play_anim=FALSE;
+BOOL play_anim = FALSE;
 
-RECT RECT16::rect32()
-{
-	RECT tmp;
-	tmp.left = left;
-	tmp.top = top;
-	tmp.right = right;
-	tmp.bottom = bottom;
-	return tmp;
+RECT RECT16::rect32() {
+  RECT tmp;
+  tmp.left = left;
+  tmp.top = top;
+  tmp.right = right;
+  tmp.bottom = bottom;
+  return tmp;
 }
 
-RECT16::RECT16 (const RECT & tmp)
-{
-	left = tmp.left;
-	top = tmp.top;
-	right = tmp.right;
-	bottom = tmp.bottom;
+RECT16::RECT16(const RECT &tmp) {
+  left = tmp.left;
+  top = tmp.top;
+  right = tmp.right;
+  bottom = tmp.bottom;
 }
 
-RECT16 & RECT16::operator=(const RECT & tmp)
-{
-	left = tmp.left;
-	top = tmp.top;
-	right = tmp.right;
-	bottom = tmp.bottom;
-	return (*this);
+RECT16 &RECT16::operator=(const RECT &tmp) {
+  left = tmp.left;
+  top = tmp.top;
+  right = tmp.right;
+  bottom = tmp.bottom;
+  return (*this);
 }
 
-POINT MAKEPOINT(LONG lParam)
-{
-	POINT point;
+POINT MAKEPOINT(LONG lParam) {
+  POINT point;
 
-	point.x = LOWORD(lParam);
-	point.y = HIWORD(lParam);
+  point.x = LOWORD(lParam);
+  point.y = HIWORD(lParam);
 
-	return point;
+  return point;
 }
 
-short get_ran (short times,short  min,short  max)
-{
-	short store;
-	short i, to_ret = 0;
+short get_ran(short times, short min, short max) {
+  short store;
+  short i, to_ret = 0;
 
-	if ((max - min + 1) == 0)
-		return 0;
-	for (i = 1; i < times + 1; i++) {
-		store = rand() % (max - min + 1);
-		to_ret = to_ret + min + store;
-		}
-	return to_ret;
+  if ((max - min + 1) == 0)
+    return 0;
+  for (i = 1; i < times + 1; i++) {
+    store = rand() % (max - min + 1);
+    to_ret = to_ret + min + store;
+  }
+  return to_ret;
 }
 
-short s_pow(short x,short y)
-{
-	return (short) pow((double) x, (double) y);
+short s_pow(short x, short y) { return (short)pow((double)x, (double)y); }
+
+short dist(location p1, location p2) {
+  return (short)sqrt(
+      (double)((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)));
 }
 
-short dist(location p1,location p2)
-{
-	return (short) sqrt((double)((p1.x - p2.x) * (p1.x - p2.x) +
-							(p1.y - p2.y) * (p1.y - p2.y)));
+short minmax(short min, short max, short k) {
+  if (k < min)
+    return min;
+  if (k > max)
+    return max;
+  return k;
 }
 
-short minmax(short min,short max,short k)
-{
-	if (k < min)
-		return min;
-	if (k > max)
-		return max;
-	return k;
+short abs(short x) {
+  if (x < 0)
+    return x * -1;
+  return x;
+}
+void Delay(short val, long *dummy) {
+  long then, now, wait_val;
+
+  wait_val = (long)val;
+  wait_val = wait_val * 16;
+  then = (long)GetCurrentTime();
+  now = then;
+  while (now - then < wait_val) {
+    now = (long)GetCurrentTime();
+  }
 }
 
-short abs(short x)
-{
-	if (x < 0)
-		return x * -1;
-	return x;
-}
-void Delay(short val,long *dummy)
-{
-	long then,now,wait_val;
+void beep() {
+  long dummy;
 
-	wait_val = (long) val;
-	wait_val = wait_val * 16;
-	then = (long)GetCurrentTime();
-	now = then;
-	while (now - then < wait_val)   {
-		now = (long) GetCurrentTime();
-		}
+  MessageBeep(MB_OK);
+  Delay(200, &dummy);
 }
 
-void beep()
-{
-	long dummy;
-
-	MessageBeep(MB_OK);
-	Delay(200,&dummy);
-
-	}
-
-void SysBeep(short)
-{
-	MessageBeep(MB_ICONQUESTION);
-}
+void SysBeep(short) { MessageBeep(MB_ICONQUESTION); }
